aaea71c47a614e2bb3e28e4edad5bb0d17c1f341
kiimnj <1najin@naver.com>
2023-10-18 23:41:55 +0900

    v2

--- src/main/java/ch04/Hello.java
+++ src/main/java/ch04/Hello.java
@@ -6,22 +6,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDateTime;
 
-@WebServlet(description = "hello servlet", urlPatterns = "/calc")
-public class Calc extends HttpServlet {
-  public Calc() {
-
+@WebServlet(description = "hello servlet", urlPatterns = "/hello")
+public class Hello extends HttpServlet {
+  public Hello() {
+    super();
   }
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    resp.setContentType("text/html; charset=utf-8");
-    PrintWriter out = resp.getWriter();
-    out.append("<html><head></head><body>")
-        .append("<h2>hello world from servlet</h2><hr>")
-        .append("현재 날짜와 시간은 : " + java.time.LocalDateTime.now())
-        .append("</body></html>");
+    resp.setContentType("text/html; charset=UTF-8");
+    resp.getWriter().append("<html><head></head><body>")
+            .append("<h2>Hello Servlet</h2><hr>")
+            .append("현재 날짜와 시간은 " + java.time.LocalDateTime.now())
+            .append("</body></html>");
   }
 
   @Override
